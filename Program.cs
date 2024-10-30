36fe236ba7d1abef9e38188167856c6a41348196
PaulineDuarte <Paulineduarte1@hotmail.com>
2024-10-30 17:26:49 -0300

    Modificações do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// teste nokia 
+Console.WriteLine("Smartphone Nokia"); 
+Smartphone nokia = new Nokia(numero:"12345",modelo:"Modelo 1",imei:"1223455",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp"); 
+
+
+// teste Iphone 
+Console.WriteLine("Smartphone Iphone"); 
+Smartphone iphone = new Iphone(numero:"45466",modelo:"Modelo 2",imei:"222334",memoria:128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Uber");
+
